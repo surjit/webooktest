c2eb382919355ad70a800538b0d0d9d4e963351b
Surjit Sidhu <mail@surjitsidhu.com>
2021-04-04 21:06:28 -0600

    bootstrap vue implemented

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+window.Vue = Vue;
+
 import VueRouter from 'vue-router'
+import { BootstrapVue } from "bootstrap-vue";
 
 import App from './App.vue'
 import router from './router/routes'
 import { store } from './store'
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 window.Vue = new Vue({
     store,
